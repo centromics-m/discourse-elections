dee176a6a5b942de87461192e1b4f8e26d4c1f53
etna <damulhan@gmail.com>
2024-12-28 14:11:33 +0900

    added message on election stage change: finding_answer -> finding_winner

--- assets/javascripts/discourse/components/election-save-current-stage.js
+++ assets/javascripts/discourse/components/election-save-current-stage.js
@@ -24,12 +24,20 @@ export default class ElectionSaveCurrentStageComponent extends ElectionSave {
       original = this.topic[`election_${this.name}`];
       if (this.property === 'finding_answer' && original === 'finding_winner') {
         if (!confirm('finding_winner에서 finding_answer로 모드를 바꾸면 ' +
-          '기존에 생성된 election_poll의 투표결과가 초기화됩니다. 계속하겠습니까?')) {
+          '본문에서 election poll 이 삭제되면서, 기존에 생성된 election_poll의 투표결과(votes)가 초기화됩니다. ' +
+          '계속하겠습니까?')) {
+          return;
+        }
+      } else if (this.property === 'finding_winner' && original === 'finding_answer') {
+        // 참고: poll-plugin: polls_updater.rb#update()
+        if (!confirm("poll의 votes > 0 일 경우, 본래 '처음 5분 후에는 폴링을 변경할 수 없습니다.' " +
+          "메시지와 함께 poll options 수정이 안되며, 그로 인하여 election stage 변경에도 오류가 생지기만 무시하도록 하였습니다. " +
+          "되도록 poll options을 수정하지 않아야 합니다. 계속하겠습니까?")) {
           return;
         }
       }
-    }
 
-    super.save();
+      super.save();
+    }
   }
 }
